0f5388380817f1b3cade81f36e69d485356e11d0
briangreenedev <brian@cloversoftware.net>
2020-05-29 23:48:13 -0600

    Folder restructure of component; Parent component with routing;

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import IndexPage from "../../pages/IndexPage";
+import ContactPage from "../../pages/ContactPage";
+import {Route, Switch} from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { useTheme } from '@material-ui/core/styles';
+import {CssBaseline} from "@material-ui/core";
+
+function App() {
+  const theme = useTheme()
+  return (
+    <div className="App">
+      <CssBaseline />
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+      <div style={{marginTop: '100px'}}>
+        <Switch>
+          <Route path="/contact" component={ContactPage}/>
+          <Route path="/" component={IndexPage}/>
+        </Switch>
+      </div>
+    </div>
+  )
+}
+
+export default App;
