c2b101fbd607296ce769edfcf9b911bf07932328
briangreenedev <brian@cloversoftware.net>
2020-05-29 23:56:08 -0600

    Setup React render.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import * as serviceWorker from './helpers/serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+	  <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
